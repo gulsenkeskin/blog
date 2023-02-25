348400d8ab620f49b1abc018e2444bba82c319d2
Gülsen Keskin <gulsenkeskin2@gmail.com>
2023-02-26 00:18:41 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,15 +1,44 @@
 import express from "express";
-import postRoutes from "./routes/posts.js";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import cookieParser from "cookie-parser";
+import multer from "multer";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
+
+// CORS hatasını çözmek için Access-Control-Allow-Origin başlığı ayarlanıyor
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
 app.listen(8800, () => {
-  console.log("connected");
+  console.log("Connected!");
 });
