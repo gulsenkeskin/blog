20159f5f84cb34e9c743c77645ef9cf2413345de
Gülsen Keskin <gulsenkeskin2@gmail.com>
2023-02-25 21:35:46 +0300

    login and logout function created

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,18 +1,17 @@
 import { db } from "../db.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
   //kullanıcı zaten kayıtlı mı?
-  const q = "SELECT * FROM users WHERE email=? OR username=?";
+  const q = "SELECT * FROM users WHERE email = ? OR username = ?";
 
   db.query(q, [req.body.email, req.body.username], (err, data) => {
-    if (err) return res.json(err);
+    if (err) return res.status(500).json(err);
     if (data.length) return res.status(409).json("Kullanıcı zaten kayıtlı");
 
     //şifreyi hash'le ve kullanıcı oluştur
-    const bcrypt = require("bcrypt");
-    const saltRounds = 10;
-    const salt = bcrypt.genSaltSync(saltRounds);
+    const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.password, salt);
 
     const q = "INSERT INTO users(`username`,`email`,`password`) VALUES (?)";
@@ -20,12 +19,48 @@ export const register = (req, res) => {
     const values = [req.body.username, req.body.email, hash];
 
     db.query(q, [values], (err, data) => {
-      if (err) return res.json(err);
-      return res.status(200).json("Kullanıcı oluşturma işlemi başarılı");
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("User has been created.");
     });
   });
 };
 
-export const login = (req, res) => {};
+export const login = (req, res) => {
+  //kullanıcı  var mı
 
-export const logout = (req, res) => {};
+  const q = "SELECT *FROM users WHERE username = ?";
+
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.status(500).json(err);
+    if (data.length === 0) return res.status(404).json("Kullanıcı bulunamadı");
+
+    //şifre kontrolü
+    const isPasswordCorrect = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+
+    if (!isPasswordCorrect)
+      return res.status(400).json("Yanlış kullanıcı adı veya şifre!");
+
+    const token = jwt.sign({ id: data[0].id }, "jwtkey");
+    const { password, ...other } = data[0];
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(other);
+  });
+};
+
+export const logout = (req, res) => {
+  res
+    .clearCookie("access_token", {
+      sameSite: "none",
+      secure: true,
+    })
+    .status(200)
+    .json("Kullanıcı çıkış yaptı.");
+};
