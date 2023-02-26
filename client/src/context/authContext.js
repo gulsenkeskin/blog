ffaa5c38e63652ea2873e983cb5be768977b7e2c
Gülsen Keskin <gulsenkeskin2@gmail.com>
2023-02-26 08:55:10 +0300

    Login user's info seted authcontext

---
+++ client/src/context/authContext.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { createContext } from "react";
+import { BaseUrl } from "../common/constants";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user") || null)
+  );
+
+  const login = async (inputs) => {
+    const res = await axios.post(`${BaseUrl}auth/login`, inputs);
+    setCurrentUser(res.data);
+  };
+
+  const logout = async (inputs) => {
+    await axios.post(`${BaseUrl}auth/logout`, inputs);
+    setCurrentUser(null);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(currentUser));
+  }, [currentUser]);
+
+  return (
+    <AuthContext.Provider value={{ currentUser, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { BaseUrl } from "../common/constants";
+import { AuthContext } from "../context/authContext";
 
 const Login = () => {
   const [inputs, setInputs] = useState({
@@ -12,6 +13,8 @@ const Login = () => {
 
   const navigate = useNavigate();
 
+  const { login, currentUser } = useContext(AuthContext);
+
   const handleChange = (e) => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
@@ -19,7 +22,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post(`${BaseUrl}auth/login`, inputs);
+      await login(inputs);
       navigate("/");
     } catch (error) {
       setError(error.response.data);
