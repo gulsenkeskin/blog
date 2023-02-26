39fdc9844912c6b59910712071557b7860fd6e94
Gülsen Keskin <gulsenkeskin2@gmail.com>
2023-02-26 08:49:03 +0300

    connection error message showed

--- api/db.js
+++ api/db.js
@@ -1,8 +1,21 @@
-import mysql from "mysql";
+import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
 
-export const db = mysql.createConnection({
+const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: process.env.DB_KEY,
   database: "blog",
 });
+
+db.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + db.threadId);
+});
+
+export { db };
