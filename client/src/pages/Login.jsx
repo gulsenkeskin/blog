bf284b489f06cdd796e87e09003d52ea80a7dfc0
Gülsen Keskin <gulsenkeskin2@gmail.com>
2023-02-26 08:48:34 +0300

    login methos created

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,15 +1,49 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BaseUrl } from "../common/constants";
 
 const Login = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+  const [err, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(`${BaseUrl}auth/login`, inputs);
+      navigate("/");
+    } catch (error) {
+      setError(error.response.data);
+    }
+  };
+
   return (
     <div className="auth">
       <h1>Giriş Yap</h1>
       <form>
-        <input type="text" placeholder="kullanıcı adı"></input>
-        <input type="password" placeholder="şifre"></input>
-        <button>Giriş Yap</button>
-        <p>Kullanıcı bulunamadı</p>
+        <input
+          name="username"
+          onChange={handleChange}
+          type="text"
+          placeholder="kullanıcı adı"
+        ></input>
+        <input
+          name="password"
+          onChange={handleChange}
+          type="password"
+          placeholder="şifre"
+        ></input>
+        <button onClick={handleSubmit}>Giriş Yap</button>
+        {err && <p>{err}</p>}
         <span>
           Hesabınız yok mu? <Link to="/register">Kayıt ol</Link>
         </span>
